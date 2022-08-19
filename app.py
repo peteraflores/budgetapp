979336122e0e394b064769bad2889122406e3095
PETREKILLAH <Japanboysushi@gmail.com>
2022-08-19 15:24:43 -0400

    testing db connection

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
 import os
+from mysql.connector import connect, Error
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+
     return "Hello World!"
+
+@app.route('/poop')
+def poop():
+    return os.environ['DB_NAME']
+    # try:
+    #     with connect(
+    #         host="localhost",
+    #         user=input("Enter username: "),
+    #         password=getpass("Enter password: "),
+    #     ) as connection:
+    #         print(connection)
+    # except Error as e:
+    #     print(e)
+
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
